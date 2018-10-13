a98006603afedf6811e1fda93910b9fe2782d45c
Clement Ferey <clement.ferey@gmail.com>
2018-10-13 22:00:18 +0200

    Handle error when socket doesn't exist

--- lib/app.rb
+++ lib/app.rb
@@ -16,6 +16,16 @@ post '/colors' do
   color = params['color']
 
   message = { type: 'color', data: color }
-  s = UNIXSocket.new("/tmp/sock")
-  s.send(message.to_json, 0)
+  begin
+    s = UNIXSocket.new('/tmp/sock')
+    s.send(message.to_json, 0)
+  rescue Errno::ENOENT => e
+    logger.error(e.message)
+
+    e.backtrace.each do |line|
+      logger.error(line)
+    end
+
+    halt 500
+  end
 end
