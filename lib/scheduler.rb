ceae9e4d6d3d296847b9facb3a07f46b17798494
Clement Ferey <clement.ferey@gmail.com>
2018-10-25 20:23:20 +0200

    Fix: require led_strip where needed

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -2,10 +2,11 @@ require 'clockwork'
 require 'json'
 require 'socket'
 require 'active_support/time'
+require_relative 'app/led_strip'
 
 module Clockwork
     every(1.day, 'Wake up', :at => '07:10', tz: 'Europe/Amsterdam', :if => lambda { |t| t.wday != 6 && t.wday != 7 }) do
       message = { type: 'wake_up', data: {} }
-      LedStrip.send_message(message)
+      ::LedStrip.send_message(message)
     end
 end
