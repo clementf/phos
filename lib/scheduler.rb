5e0baca3ce2f5bb47d9c100b51f7a1b647d0c32c
Clement Ferey <clement.ferey@gmail.com>
2018-10-28 17:32:42 +0100

    Fix: 0 is actually sunday

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -5,7 +5,7 @@ require 'active_support/time'
 require_relative 'app/led_strip'
 
 module Clockwork
-    every(1.day, 'Wake up', :at => '07:10', tz: 'Europe/Amsterdam', :if => lambda { |t| t.wday != 6 && t.wday != 7 }) do
+    every(1.day, 'Wake up', :at => '07:10', tz: 'Europe/Amsterdam', :if => lambda { |t| t.wday != 6 && t.wday != 0 }) do
       message = { type: 'wake_up', data: {} }
       ::LedStrip.send_message(message)
     end
