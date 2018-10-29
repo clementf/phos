bcb7aabf9ddb62d1356b39e899bf6de94bce009f
Clement Ferey <clement.ferey@gmail.com>
2018-10-29 21:49:47 +0100

    Add specs for current color
    - Fix: require sinatra/json

--- lib/app/phos.rb
+++ lib/app/phos.rb
@@ -2,6 +2,7 @@
 
 require 'sucker_punch' # has to be before requiring sinatra
 require 'sinatra/base'
+require "sinatra/json"
 require 'sinatra/logger'
 require 'json'
 
---
+++ spec/request/color_spec.rb
@@ -0,0 +1,27 @@
+require 'app/phos'
+require 'app/color'
+require 'app/controllers/color_controller'
+require 'spec_helper'
+
+RSpec.describe ColorController do
+  let(:app) { ColorController.new }
+
+  context 'No current color is set' do
+    let(:response) { get '/current' }
+
+    it 'status is 200' do
+      expect(response).to be_ok
+    end
+
+    it 'gives json' do
+      expect(response.content_type).to eq 'application/json'
+    end
+
+    it 'give default black color' do
+      color = JSON.parse(response.body)
+      expect(color['r']).to eq 0
+      expect(color['g']).to eq 0
+      expect(color['b']).to eq 0
+    end
+  end
+end
