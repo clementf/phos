58e4e9b5e7636d94c53b13ef981471c91f313abd
Clement Ferey <clement.ferey@gmail.com>
2018-11-04 17:22:48 +0100

    Move dependencies to dev dependencies

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,6 @@
 const config = require('./webpack.config.js');
 const webpack = require('webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 config.mode = 'production';
 
@@ -10,8 +9,7 @@ config.optimization = {
 };
 
 config.plugins.push(
-  new CompressionPlugin(),
-  new BundleAnalyzerPlugin()
+  new CompressionPlugin()
 );
 
 module.exports = config;
