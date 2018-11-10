dfa62712ee04371650b9b2ef5183a2ce3669cfc5
Clement Ferey <clement.ferey@gmail.com>
2018-11-10 19:40:38 +0100

    Basic crud for alarms

--- lib/app/controllers/alarm_controller.rb
+++ lib/app/controllers/alarm_controller.rb
@@ -3,3 +3,33 @@ class AlarmController < Phos
     json Alarm.all.order(updated_at: :desc)
   end
 
+  post '' do
+    data = HashWithIndifferentAccess.new(JSON.parse(@request.body.read))
+
+    Alarm.create!(alarm_params(data))
+
+    json alarm
+  end
+
+  put '/:id' do
+    data = HashWithIndifferentAccess.new(JSON.parse(@request.body.read))
+    alarm = Alarm.find(params[:id])
+    alarm.update!(alarm_params(data))
+
+    json alarm
+  end
+
+  delete '/:id' do
+    Alarm.delete(params[:id])
+  end
+
+  private
+
+  def alarm_params(params)
+    {
+      time:   Time.new(2000, 1, 1, params[:hour], params[:min], 0),
+      active: params[:active] == 'true',
+      days:   params[:days]
+    }
+  end
+end
--- lib/app/models/alarm.rb
+++ lib/app/models/alarm.rb
@@ -2,11 +2,23 @@
 
 class Alarm < ActiveRecord::Base
   serialize :days, Array
+  before_save :sanitize_days
 
   def as_json(*)
     super.except('time', 'created_at', 'updated_at').tap do |alarm|
       alarm['hour'] = time.getlocal.hour
       alarm['min'] = time.getlocal.min
     end
+  end
+
+  private
+
+  def sanitize_days
+    dedup_days
+    days.select! { |day| day.between?(0, 6) }
+  end
+
+  def dedup_days
+    days.uniq!
   end
 end
