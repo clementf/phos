ca75d51b22363e10abd3581ceb493ec0457b4272
Clement Ferey <clement.ferey@gmail.com>
2018-11-10 19:03:07 +0100

    Filter params when rendering json

--- lib/app/models/alarm.rb
+++ lib/app/models/alarm.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 class Alarm < ActiveRecord::Base
   serialize :days, Array
+
+  def as_json(*)
+    super.except('time', 'created_at', 'updated_at').tap do |alarm|
+      alarm['hour'] = time.hour
+      alarm['min'] = time.min
+    end
+  end
 end
