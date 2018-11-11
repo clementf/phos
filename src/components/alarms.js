1584331405129f0f31cc3fb6167e9a957212528a
Clement Ferey <clement.ferey@gmail.com>
2018-11-11 12:47:57 +0100

    List alarms on alarms page

--- src/components/alarms.js
+++ src/components/alarms.js
@@ -4,6 +4,13 @@ import { Link } from "react-router-dom";
 import client from './client.js';
 
 class Alarms extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      alarms: []
+    };
+  }
+
   componentDidMount() {
     const reactContext = this;
 
@@ -14,9 +21,17 @@ class Alarms extends React.Component {
   }
 
   render() {
+    const alarms = []
+    const listAlarms = this.state.alarms.map((alarm) =>
+      <li key={alarm.id}>{`${alarm.hour}:${alarm.min}`}</li>
+    );
     return (
       <div>
         <h2>Alarms</h2>
+        <ul>
+          {listAlarms}
+        </ul>
+
         <ul>
           <li>
             <Link to="/">Home</Link>
