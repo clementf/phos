b11490337678ed552ccfd6ec35470e6bcdcc3a48
Clement Ferey <clement.ferey@gmail.com>
2018-11-17 10:49:08 +0100

    Fix CORS setup

--- lib/app/cors.rb
+++ lib/app/cors.rb
@@ -7,7 +7,7 @@ module Sinatra
       end
 
       app.options '*' do
-        response.headers['Allow'] = 'HEAD,GET,PUT,POST,DELETE,OPTIONS'
+        response.headers['Access-Control-Allow-Methods'] = 'HEAD,GET,PUT,POST,DELETE,OPTIONS'
 
         headers_allowed = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept'
         response.headers['Access-Control-Allow-Headers'] = headers_allowed
