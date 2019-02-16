4dd039d07bbf34dfc59b8e5b752ac82cdc80bf75
Clement Ferey <clement.ferey@gmail.com>
2019-02-16 10:17:48 +0100

    Make sure event is required

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -6,14 +6,16 @@ require 'socket'
 require 'active_support/time'
 require_relative 'app/phos'
 require_relative 'app/led_strip'
+require_relative 'app/models/event'
 require_relative 'app/alarm_checker'
 
 module Clockwork
   every(1.minute, 'Wake up') do
+
     if AlarmChecker.new(Time.now).should_wake_up?
       message = { type: 'wake_up', data: {} }
       ::LedStrip.send_message(message)
-      Event.create!(name: 'waking_up')
+      ::Event.create!(name: 'waking_up')
     end
   end
 end
