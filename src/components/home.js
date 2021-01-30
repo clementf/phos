b7688c0e785713061c3601b89c46b1122b7dc7ab
Clement Ferey <clement.ferey@gmail.com>
2021-01-30 18:33:01 +0100

    Only display christmas button in December :)

--- src/components/home.js
+++ src/components/home.js
@@ -18,22 +18,22 @@ class Home extends React.Component {
   componentDidMount() {
     const reactContext = this;
 
-    client.get('/colors/current').then(function(response) {
+    client.get('/colors/current').then(function (response) {
       const color = response.data;
       reactContext.setState({ color: color });
 
       var demoColorPicker = new iro.ColorPicker('#color-picker-container', {
         width: 240,
         height: 240,
-        color: color
+        color: color,
       });
 
-      demoColorPicker.on('input:end', function(color) {
+      demoColorPicker.on('input:end', function (color) {
         color = color['rgb'];
         reactContext.setState({ color: color });
 
         client.post('/colors', {
-          color: color
+          color: color,
         });
       });
     });
@@ -42,25 +42,44 @@ class Home extends React.Component {
   dimmer() {
     client
       .post('/modes', {
-        mode: 'dimmer'
+        mode: 'dimmer',
       })
-      .then(response => {
+      .then((response) => {
         openSnackbar({ message: 'Alright, good night!' });
       });
   }
 
-  christmas() {
+  christmasMode() {
     client
       .post('/modes', {
-        mode: 'christmas'
+        mode: 'christmas',
       })
-      .then(response => {
+      .then((response) => {
         openSnackbar({ message: 'Jingle bells!' });
       });
   }
+
+  isItDecember() {
+    return new Date().getMonth() == 11;
+  }
+
   render() {
     const { classes } = this.props;
+    let christmasButton;
 
+    if (this.isItDecember()) {
+      christmasButton = (
+        <Button
+          variant="contained"
+          onClick={this.christmasMode}
+          className={classes.button}
+        >
+          Christmas theme
+        </Button>
+      );
+    } else {
+      christmasButton = '';
+    }
     return (
       <div>
         <div id="color-picker-container" />
@@ -71,23 +90,17 @@ class Home extends React.Component {
         >
           Dimmer
         </Button>
-        <Button
-          variant="contained"
-          onClick={this.christmas}
-          className={classes.button}
-        >
-          Christmas theme
-        </Button>
+        {christmasButton}
         <Notifier />
       </div>
     );
   }
 }
 
-const styles = theme => ({
+const styles = (theme) => ({
   button: {
     display: 'flex',
-    margin: '10px auto'
-  }
+    margin: '10px auto',
+  },
 });
 export default withStyles(styles)(Home);
